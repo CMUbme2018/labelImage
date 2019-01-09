eb1ba5f7a02d416ac23f1e9dacf8f150972e1582
wangl <603020460@qq.com>
2019-01-09 10:34:29 +0800

    基本界面与读图功能

--- labelImage/labelImage/MainWindow.xaml.cs
+++ labelImage/labelImage/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace labelImage
 {
@@ -24,5 +25,131 @@ namespace labelImage
         {
             InitializeComponent();
         }
+
+        #region 打开图像
+        string directory = null;
+        string imgPath = null;
+        List<string> imgArray = null;
+        public string ImageSourceFileName = null;  //参考图像的路径文件名
+        public string ImageTargetFileName = null;   //浮动图像的路径文件名
+
+        //Ribbon界面，打开参考图像按钮
+        private void RBOpenSourceImage_Click(object sender, RoutedEventArgs e)
+        {
+            ImageSourceFileName = OpenImage("打开参考图像");
+            if (ImageSourceFileName != "")
+            {
+                ImageSourceImage.Source = new BitmapImage(new Uri(ImageSourceFileName));
+            }
+        }
+
+        //打开一个图像文件
+        private string OpenImage(string ofdTitle)
+        {
+            System.Windows.Forms.OpenFileDialog openDlg = new System.Windows.Forms.OpenFileDialog();
+            openDlg.Filter = "JPEG图片|*.jpg; *.jpeg|" + "TIFF图片|*.tif; *.tiff|" + "BMP|*.bmp|" + "PNG|*.png|" + "所有文件|*.*";
+            openDlg.Title = ofdTitle;
+            openDlg.Multiselect = true;
+            openDlg.ShowHelp = true;
+            openDlg.ShowDialog();
+            string fileName = openDlg.FileName;
+            return fileName;
+        }
+        #endregion
+
+        #region 鼠标操作
+        System.Windows.Point PreviousMousePoint;
+        bool IsMouseLeftButtonDown;
+        private void ImageLeft_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var img = sender as ContentControl;
+            if (img == null)
+            {
+                return;
+            }
+            this.Cursor = System.Windows.Input.Cursors.ScrollAll;
+            img.CaptureMouse();
+            IsMouseLeftButtonDown = true;
+            PreviousMousePoint = e.GetPosition(img);
+        }
+        private void ImageLeft_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var img = sender as ContentControl;
+            if (img == null)
+            {
+                return;
+            }
+            this.Cursor = System.Windows.Input.Cursors.Arrow;
+            img.ReleaseMouseCapture();
+            IsMouseLeftButtonDown = false;
+        }
+        private void ImageLeft_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var img = sender as ContentControl;
+            if (img == null)
+            {
+                return;
+            }
+            if (IsMouseLeftButtonDown)
+            {
+                DoImageMove(img, e);
+            }
+        }
+        private void ImageLeft_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ContentControl image = sender as ContentControl;
+            if (image == null)
+            {
+                return;
+            }
+            TransformGroup group = ImageGird.FindResource("ImageTransformResource") as TransformGroup;
+            System.Windows.Point point = e.GetPosition(image);
+            double scale = e.Delta * 0.01;
+            ZoomImage(group, point, scale);
+        }
+        #endregion
+
+        #region 图像操作
+        private static void ZoomImage(TransformGroup group, System.Windows.Point point, double scale)
+        {
+            System.Windows.Point pointToContent = group.Inverse.Transform(point);
+            ScaleTransform transform = group.Children[0] as ScaleTransform;
+            if (transform.ScaleX + scale < 1)
+            {
+                return;
+            }
+            transform.ScaleX += scale;
+            transform.ScaleY += scale;
+            TranslateTransform transform1 = group.Children[1] as TranslateTransform;
+            transform1.X = -1 * ((pointToContent.X * transform.ScaleX) - point.X);
+            transform1.Y = -1 * ((pointToContent.Y * transform.ScaleY) - point.Y);
+        }
+        private void DoImageMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            ContentControl image = sender as ContentControl;
+            if (image == null)
+            {
+                return;
+            }
+            TransformGroup group = ImageGird.FindResource("ImageTransformResource") as TransformGroup;
+            TranslateTransform transform = group.Children[1] as TranslateTransform;
+            System.Windows.Point position = e.GetPosition(image);
+            transform.X += position.X - PreviousMousePoint.X;
+            transform.Y += position.Y - PreviousMousePoint.Y;
+            PreviousMousePoint = position;
+        }
+        #endregion
+
+        #region Ribbon按钮操作
+        private void RBRemarksImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RBExportXML_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
     }
 }
